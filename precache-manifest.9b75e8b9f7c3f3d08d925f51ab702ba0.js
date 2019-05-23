9273510ba933689dec702f8ecd9f7fd3556cbaac
Francisco Javier Barrera Rodriguez <francisco.barrera@uptc.edu.co>
2019-05-22 23:01:42 -0500

    build

---
+++ precache-manifest.9b75e8b9f7c3f3d08d925f51ab702ba0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb26da0be049034e9b2adef5e84c634a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db073502cbda7406e1a4",
+    "url": "/static/css/main.814b800c.chunk.css"
+  },
+  {
+    "revision": "c2b3d76474890d62dc2f",
+    "url": "/static/js/2.7af4507b.chunk.js"
+  },
+  {
+    "revision": "db073502cbda7406e1a4",
+    "url": "/static/js/main.c980f00b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3ae75bc7a1131199493054d2fdd5470b",
+    "url": "/static/media/banner_1.3ae75bc7.png"
+  }
+]);
