3e27a262a809d9d267bb201ed45ad48800d568e9
Francisco Barrera <francisco.barrera@uptc.edu.co>
2019-06-01 15:45:31 -0500

    deploy

--- precache-manifest.42aacfa9b18f806eceb82c6dcf0d2c30.js
+++ precache-manifest.42aacfa9b18f806eceb82c6dcf0d2c30.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2ac52f16cf01ec7a182cf2909949479f",
+    "revision": "0ba761046d07fbf58763428d3e87648d",
     "url": "/baraton-build/index.html"
   },
   {
-    "revision": "fd330b29aa57c50cf930",
-    "url": "/baraton-build/static/css/main.26f16639.chunk.css"
+    "revision": "c03842c185c1956f49e7",
+    "url": "/baraton-build/static/css/main.6af95ff8.chunk.css"
   },
   {
-    "revision": "c2b3d76474890d62dc2f",
-    "url": "/baraton-build/static/js/2.7af4507b.chunk.js"
+    "revision": "e92b2494e2d261aafc1c",
+    "url": "/baraton-build/static/js/2.a0f1c262.chunk.js"
   },
   {
-    "revision": "fd330b29aa57c50cf930",
-    "url": "/baraton-build/static/js/main.9e132d84.chunk.js"
+    "revision": "c03842c185c1956f49e7",
+    "url": "/baraton-build/static/js/main.4f57292b.chunk.js"
   },
   {
     "revision": "848de0c6d1e534916591",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.582977193fd647060cac9edc304e5d10.js"
+  "/baraton-build/precache-manifest.42aacfa9b18f806eceb82c6dcf0d2c30.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/baraton-build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
